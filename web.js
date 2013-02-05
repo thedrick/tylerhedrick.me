f17f829ae6099573b697f8be0e0d7638ccc2bca5
Tyler Hedrick <tylerhedrick@me.com>
2013-02-05 13:09:08 -0500

    updating server to send back index

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/resources'));
 
 app.get('/', function(request, response) {
   response.sendfile('./index.html');
